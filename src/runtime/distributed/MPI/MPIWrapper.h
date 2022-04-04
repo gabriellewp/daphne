56978e9a0523cf8e7a9a7cc8d1e2b1233a53e5dc
Gabrielle Poerwawinata <poerwa0000@dmi-borrow12.dmi.unibas.ch>
2022-04-04 16:28:33 +0200

    trying things

--- src/runtime/distributed/MPI/MPIWrapper.h
+++ src/runtime/distributed/MPI/MPIWrapper.h
@@ -191,24 +191,148 @@ inline int MPIWrapper::execute(const char *mlirCode,
 
     }
 
-    //do the computation
+    //do the computation, this function can only start if we know the mlir code... 
     doComputation();
 
-    //do collection
-    
+    //do results collection
+    MPI_Datatype subRows;
+    int ** arrlocal;
+    arrlocal = new int * [rowChunkSize]; //allocate rows
+    MPI_Type_vector(rowChunkSize, col, col, MPI_INT, &subRows);
+    MPI_Type_commit(&subRows);
+    MPI_Gather(&(arrlocal[0][0]),  rowChunkSize*col, MPI_INT, &(arrglobal[0][0]), 1, subRows, recvRank, MPI_COMM_WORLD);
+
+
     
 }
 
-inline int MPIWrapper::doComputation(){
+inline int MPIWrapper::doComputation(const ::distributed::Task *request,
+                                 ::distributed::ComputeResult *response){
     distributed::Task task;
     for (size_t i = 0; i < numInputs; i++) {
         auto map =  args[i]->dataPlacement.getMap();
         *task.add_inputs()->mutable_stored() = map[addr].getData();
     }
     task.set_mlir_code(mlirCode);
+    //task actually the request here
     //StoredInfo storedInfo ({addr, nullptr});
-    // i dont understand what is the asynccomputecall doing
-    caller.asyncComputeCall(addr, storedInfo, task);
+    //look into compute in WorkerImpl.cpp (DistributedICCS)
+    DaphneUserConfig cfg;
+    cfg.use_vectorized_exec = true;
+    // TODO Decide if vectorized pipelines should be used on this worker.
+    // TODO Decide if selectMatrixReprs should be used on this worker.
+    // TODO Once we hand over longer pipelines to the workers, we might not
+    // want to hardcode insertFreeOp to false anymore. But maybe we will insert
+    // the FreeOps at the coordinator already.
+    DaphneIrExecutor executor(false, false, cfg);
+
+    mlir::OwningModuleRef module(mlir::parseSourceString<mlir::ModuleOp>(request->mlir_code(), executor.getContext()));
+    if (!module) {
+        auto message = "Failed to parse source string.\n";
+        llvm::errs() << message;
+        return -1;
+        //return ::grpc::Status(::grpc::StatusCode::ABORTED, message);
+    }
+
+    auto *distOp = module->lookupSymbol(DISTRIBUTED_FUNCTION_NAME);
+    mlir::FuncOp distFunc;
+    if (!(distFunc = llvm::dyn_cast_or_null<mlir::FuncOp>(distOp))) {
+        auto message = "MLIR fragment has to contain `dist` FuncOp\n";
+        llvm::errs() << message;
+        return -1;
+        //return ::grpc::Status(::grpc::StatusCode::ABORTED, message);
+    }
+    auto distFuncTy = distFunc.getType();
+
+    std::vector<void *> inputs;
+    std::vector<void *> outputs;
+    auto packedInputsOutputs = createPackedCInterfaceInputsOutputs(distFuncTy,
+        request->inputs(),
+        outputs,
+        inputs);
+    
+    // Increase the reference counters of all inputs to the `dist` function.
+    // (But only consider data objects, not scalars.)
+    // This is necessary to avoid them from being destroyed within the
+    // function. Note that this increasing is in-line with the treatment of
+    // local function calls, where we also increase the inputs' reference
+    // counters before the call, for the same reason. See ManageObjsRefsPass
+    // for details.
+    for(size_t i = 0; i < inputs.size(); i++)
+        // TODO Use CompilerUtils::isObjType() once this branch has been rebased.
+        // if(CompilerUtils::isObjType(distFuncTy.getInput(i)))
+        if(distFuncTy.getInput(i).isa<mlir::daphne::MatrixType, mlir::daphne::FrameType>())
+            reinterpret_cast<Structure*>(inputs[i])->increaseRefCounter();
+
+    // Execution
+    // TODO Before we run the passes, we should insert information on shape
+    // (and potentially other properties) into the types of the arguments of
+    // the DISTRIBUTED_FUNCTION_NAME function. At least the shape can be
+    // obtained from the cached data partitions in localData_. Then, shape
+    // inference etc. should work within this function.
+    if (!executor.runPasses(module.get())) {
+        std::stringstream ss;
+        ss << "Module Pass Error.\n";
+        // module->print(ss, llvm::None);
+        llvm::errs() << ss.str();
+        return -1;
+        //return ::grpc::Status(::grpc::StatusCode::ABORTED, ss.str());
+    }
+
+    mlir::registerLLVMDialectTranslation(*module->getContext());
+
+    auto engine = executor.createExecutionEngine(module.get());
+    if (!engine) {
+        return ::grpc::Status(::grpc::StatusCode::ABORTED, "Failed to create JIT-Execution engine");
+    }
+    auto error = engine->invokePacked(DISTRIBUTED_FUNCTION_NAME,
+        llvm::MutableArrayRef<void *>{&packedInputsOutputs[0], (size_t)0});
+
+    if (error) {
+        std::stringstream ss("JIT-Engine invocation failed.");
+        llvm::errs() << "JIT-Engine invocation failed: " << error << '\n';
+        //return ::grpc::Status(::grpc::StatusCode::ABORTED, ss.str());
+        return -1;
+    }
+
+    for (auto zipped : llvm::zip(outputs, distFuncTy.getResults())) {
+        auto output = std::get<0>(zipped);
+        auto type = std::get<1>(zipped);
+
+        auto identification = "tmp_" + std::to_string(tmp_file_counter_++);
+        localData_[identification] = output;
+
+        distributed::WorkData::DataCase dataCase = dataCaseForType(type);
+
+        distributed::WorkData workData;
+        switch (dataCase) {
+        case distributed::WorkData::kStored: {
+            auto matTy = type.dyn_cast<mlir::daphne::MatrixType>();
+            if(matTy.getElementType().isa<mlir::Float64Type>()){
+                auto mat = static_cast<Matrix<double> *>(output);
+                workData.mutable_stored()->set_num_rows(mat->getNumRows());
+                workData.mutable_stored()->set_num_cols(mat->getNumCols());
+                if (matTy.getRepresentation() == mlir::daphne::MatrixRepresentation::Sparse)
+                    workData.mutable_stored()->set_type(distributed::StoredData::Type::StoredData_Type_CSRMatrix_f64);
+                else
+                    workData.mutable_stored()->set_type(distributed::StoredData::Type::StoredData_Type_DenseMatrix_f64);
+            } else {
+                auto mat = static_cast<Matrix<int64_t> *>(output);
+                workData.mutable_stored()->set_num_rows(mat->getNumRows());
+                workData.mutable_stored()->set_num_cols(mat->getNumCols());
+                if (matTy.getRepresentation() == mlir::daphne::MatrixRepresentation::Sparse)
+                    workData.mutable_stored()->set_type(distributed::StoredData::Type::StoredData_Type_CSRMatrix_i64);
+                else
+                    workData.mutable_stored()->set_type(distributed::StoredData::Type::StoredData_Type_DenseMatrix_i64);
+            }
+            workData.mutable_stored()->set_filename(identification);
+            break;
+        }
+        default: assert(false);
+        }
+        *response->add_outputs() = workData;
+    }
+    
 }
 //we will have our definition of broadcast, collect, send and receive here. so replace runMPI with individual functionalities
 
--- test/runtime/distributed/worker/MPITest.cpp
+++ test/runtime/distributed/worker/MPITest.cpp
@@ -2,14 +2,45 @@
 #include <stdio.h>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    int rank, size;
+const std::string dirPath = "test/runtime/distributed/worker/";
 
-    MPI_Init (&argc, &argv);	/* starts MPI */
-    MPI_Comm_rank (MPI_COMM_WORLD, &rank);	/* get current process id */
-    MPI_Comm_size (MPI_COMM_WORLD, &size);	/* get number of processes */
-    printf( "Hello world from process %d of %d\n", rank, size );
-    MPI_Finalize();
-    return 0;
+//this maybe very similar to the one in grpc
+TEMPLATE_PRODUCT_TEST_CASE("Simple distributed worker functionality test", TAG_DISTRIBUTED, (DenseMatrix), (double))
+{
+    using DT = TestType;
+    using VT = typename DT::VT;
+    MPIWrapper mpiWrapper;
+    WHEN ("Sending a task where no outputs are expected")
+    {
+        distributed::Task task;
+        task.set_mlir_code("func @" + WorkerImpl::DISTRIBUTED_FUNCTION_NAME +
+            "() -> () {\n"
+            "  \"daphne.return\"() : () -> ()\n"
+            "}\n");
+        //where is the indication that the data should be distributed/broadcasted    
+        int status = mpiWrapper.execute(task, result); 
+        
+        cout << "status: " << status << endl;
+    }
 
+    WHEN("Sending simple random generation task")
+    {
+        distributed::Task task;
+        task.set_mlir_code("func @" + WorkerImpl::DISTRIBUTED_FUNCTION_NAME +
+            "() -> !daphne.Matrix<?x?xf64> {\n"
+            "    %3 = \"daphne.constant\"() {value = 2 : si64} : () -> si64\n"
+            "    %4 = \"daphne.cast\"(%3) : (si64) -> index\n"
+            "    %5 = \"daphne.constant\"() {value = 3 : si64} : () -> si64\n"
+            "    %6 = \"daphne.cast\"(%5) : (si64) -> index\n"
+            "    %7 = \"daphne.constant\"() {value = 1.000000e+02 : f64} : () -> f64\n"
+            "    %8 = \"daphne.constant\"() {value = 2.000000e+02 : f64} : () -> f64\n"
+            "    %9 = \"daphne.constant\"() {value = 1.000000e+00 : f64} : () -> f64\n"
+            "    %10 = \"daphne.constant\"() {value = -1 : si64} : () -> si64\n"
+            "    %11 = \"daphne.randMatrix\"(%4, %6, %7, %8, %9, %10) : (index, index, f64, f64, f64, si64) -> !daphne.Matrix<?x?xf64>"
+            "    \"daphne.return\"(%11) : (!daphne.Matrix<?x?xf64>) -> ()\n"
+            "  }");
+
+        int status = mpiWrapper.execute(task, result);
+        cout << "status: " << status << endl;
+    }
 }
